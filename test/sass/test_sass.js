e60db47d86c390083724787db95b849bd97d35ca
Jonny Gerig Meyer <jonny@oddbird.net>
2017-09-14 12:37:00 -0400

    Update dependencies.

--- test/sass/test_sass.js
+++ test/sass/test_sass.js
@@ -1,9 +1,11 @@
 /* eslint-env mocha */
 
-var path = require('path');
-var sassTrue = require('sass-true');
+'use strict';
 
-var importer = function(url) {
+const path = require('path');
+const sassTrue = require('sass-true');
+
+const importer = function(url) {
   if (url[0] === '~') {
     url = path.resolve('node_modules', url.substr(1));
   }
@@ -11,5 +13,5 @@ var importer = function(url) {
   return { file: url };
 };
 
-var sassFile = path.join(__dirname, 'test.scss');
-sassTrue.runSass({ importer: importer, file: sassFile }, describe, it);
+const sassFile = path.join(__dirname, 'test.scss');
+sassTrue.runSass({ importer, file: sassFile }, describe, it);
