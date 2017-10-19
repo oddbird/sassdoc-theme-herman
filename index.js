e2b8c623c7f66c14c170124649a54c2e2fa9f668
Jonny Gerig Meyer <jonny@oddbird.net>
2017-10-19 16:42:04 -0400

    Add all `@font` HTML to `@example` <head>.

--- index.js
+++ index.js
@@ -654,8 +654,11 @@ herman.annotations = [
    * NOTE: Locally-hosted fonts require global `fontpath` Herman setting.
    */
   function font(env) {
+    // Matches first occurence of text within `''` or `""`
     const keyRE = /(["'])(?:(?=(\\?))\2.)*?\1/;
+    // Matches text within `()`
     const variantsRE = /\(([^)]*)\)/;
+    // Matches text within `{}`
     const formatsRE = /{([^}]*)}/;
     return {
       name: 'font',
@@ -679,9 +682,12 @@ herman.annotations = [
         const linebreak = raw.indexOf('\n');
         let args = raw;
         if (linebreak > -1) {
+          // Multiline annotation; separate first-line args from following HTML
           args = raw.substr(0, linebreak);
           const html = raw.substr(linebreak + 1);
           obj.html = stripIndent(html.replace(/^\n|\n$/g, ''));
+          // Concatenate all font HTML to include in `@example` annotations
+          env.fontsHTML = `${env.fontsHTML || ''}\n${obj.html}`;
         }
         const keyBits = args.match(keyRE);
         if (!keyBits) {
@@ -689,6 +695,7 @@ herman.annotations = [
         }
         const key = keyBits[0];
         obj.key = key.substring(1, key.length - 1).trim();
+        // Strip `key` before parsing variants and formats
         args = args.substr(key.length);
         const variantsBits = args.match(variantsRE);
         if (variantsBits) {
