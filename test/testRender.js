1cc9c638e17a97fa3e43aca3445f7c9ce8b39b06
Jonny Gerig Meyer <jonny@oddbird.net>
2017-11-03 17:17:13 -0400

    Fix jstest.

--- test/testRender.js
+++ test/testRender.js
@@ -2,30 +2,20 @@
 
 const assert = require('assert');
 
-const render = require('../lib/render');
 const prepareContext = require('../lib/prepareContext');
+const render = require('../lib/render');
+const { nunjucksEnv } = require('../constants');
 
 describe('render', function() {
   it('does a render', function(done) {
-    const env = {
-      herman: {
-        customCSS: 'dist/css/main.css',
-        minifiedIcons: 'templates/_icons.svg',
-        sass: {
-          jsonfile: 'dist/css/json.css',
-        },
-      },
-      dir: __dirname,
-      renderString() {},
-    };
     const tpl = 'templates/base.j2';
-    const dest = `${__dirname}/dest`;
+    const dest = `${__dirname}/dest/base.html`;
     prepareContext({
       data: [],
       groups: {},
       display: {},
     })
-      .then(ctx => render(env, tpl, dest, ctx))
+      .then(ctx => render(nunjucksEnv, tpl, dest, ctx))
       .then(() => {
         // Look for side-effects
         assert.ok(true);
