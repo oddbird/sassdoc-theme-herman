2968c9d758fc49cb30e213897888b867f673ab66
Jonny Gerig Meyer <jonny@oddbird.net>
2017-11-06 11:18:48 -0500

    Do not use async/await.

--- test/testAssets.js
+++ test/testAssets.js
@@ -7,12 +7,14 @@ const sinon = require('sinon');
 const assets = require('../lib/assets');
 
 describe('assets', function() {
-  it('Runs the vfs pipeline', async function() {
+  it('Runs the vfs pipeline', function(done) {
     const src = '.';
     const dest = 'dest';
     const destSpy = sinon.spy(vfs, 'dest');
     // Complete the promise chain, then let's check side-effects:
-    await assets(src, dest, {});
-    assert(destSpy.calledWith(dest));
+    assets(src, dest, {}).then(() => {
+      assert(destSpy.calledWith(dest));
+      done();
+    });
   });
 });
