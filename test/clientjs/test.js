1fcab410fcd08720de415377579dbfc1652a767b
Kit La Touche <kit@transneptune.net>
2017-11-27 15:39:36 -0700

    Add at least one test

--- test/clientjs/test.js
+++ test/clientjs/test.js
@@ -1,7 +1,19 @@
 'use strict';
 
-describe('herman', function() {
-  it('does a test', function() {
-    expect(true).to.be.true;
+describe('initializeToggles', function() {
+  beforeEach(function() {
+    const id = (this.id = 'some-id');
+    var fixture =
+      `<button aria-controls="${id}" data-toggle="button">show panel</button>` +
+      `<nav data-toggle="target" data-target-id="${id}"></nav>`;
+
+    document.body.insertAdjacentHTML('afterbegin', fixture);
+  });
+
+  it('listens to toggle:close', function() {
+    const target = $(`[data-target-id="${this.id}"]`);
+    const trigger = sinon.spy(target, 'trigger');
+    $('body').trigger('toggle:close');
+    expect(trigger).to.have.been.calledWith('target:close');
   });
 });
