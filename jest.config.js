9b7495865fd9558de6932f1cc2d1b5a3468de181
Jonny Gerig Meyer <jonny@oddbird.net>
2022-11-03 14:23:51 -0400

    Remove docs/ dir from npm package

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 // For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
+// https://jestjs.io/docs/configuration
 
 'use strict';
 
