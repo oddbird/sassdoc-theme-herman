9c3e9671b18766678fae360e3fd6bbb71f3065db
Jonny Gerig Meyer <jonny@oddbird.net>
2024-02-23 12:02:44 -0500

    update true

--- test/sass/test_sass.js
+++ test/sass/test_sass.js
@@ -2,10 +2,7 @@
 
 const path = require('path');
 
-const { NodePackageImporter } = require('sass');
 const sassTrue = require('sass-true');
 
 const sassFile = path.join(__dirname, 'test.scss');
-sassTrue.runSass({ describe, it }, sassFile, {
-  importers: [new NodePackageImporter()],
-});
+sassTrue.runSass({ describe, it, sass: 'sass-embedded' }, sassFile);
